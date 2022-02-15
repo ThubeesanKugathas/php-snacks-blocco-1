cf1950d2b6a20f2fb2f2491aa53f3b24d8e18714
ThubeesanKugathas <Thubeesan@outlook.com>
2022-02-15 14:34:08 +0100

    snack 1 with cycle for function

--- snack-1.php
+++ snack-1.php
@@ -7,7 +7,44 @@ Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squ
 casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60
 */
-
+    $matches = [
+        [
+            "casa" => "Varese",
+            "ospite" => "Virtus Bologna",
+            "puntiCasa" => 80,
+            "puntiOspite" => 81
+        ],
+        [
+            "casa" => "Brindisi",
+            "ospite" => "Reggiana",
+            "puntiCasa" => 89,
+            "puntiOspite" => 75
+        ],
+        [
+            "casa" => "Fortitudo Bologna",
+            "ospite" => "Brescia",
+            "puntiCasa" => 86,
+            "puntiOspite" => 93
+        ],
+        [
+            "casa" => "Pesaro",
+            "ospite" => "Cremona",
+            "puntiCasa" => 93,
+            "puntiOspite" => 87
+        ],
+        [
+            "casa" => "Treviso",
+            "ospite" => "Tortona",
+            "puntiCasa" => 72,
+            "puntiOspite" => 70
+        ],
+        [
+            "casa" => "Trento",
+            "ospite" => "Olimpia Milano",
+            "puntiCasa" => 73,
+            "puntiOspite" => 79
+        ],
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +56,24 @@ Olimpia Milano - Cantù | 55-60
     <title>SNACK UNO</title>
 </head>
 <body>
-    
+    <h1>Partite basket giornata 19:</h1>
+    <ul>
+        <?php for ($i = 0; $i < count($matches); $i++) { ?>
+            <li><?php echo "Partita nr. " . ($i + 1) ?>
+                <ul>
+                    <li>
+                        <div class="d-flex">
+                            <div>
+                                <?php echo $matches[$i]["casa"] ?>: <?php echo $matches[$i]["puntiCasa"] ?>
+                            </div>
+                            <div>
+                                <?php echo $matches[$i]["ospite"] ?>: <?php echo $matches[$i]["puntiOspite"] ?>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+        <?php } ?>
+    </ul>
 </body>
 </html>
